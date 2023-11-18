106e6e49875e3d67551f5db7de3d0d055c660635
josealeperdomo <perdomomolinajose@gmail.com>
2023-11-18 02:03:56 -0400

    ....

--- CALCULADORA/calculadora.js
+++ CALCULADORA/calculadora.js
@@ -14,40 +14,40 @@
    Nota: Validar cada uno de estos campos con expresiones regulares.
 */
 
-let pantalla = document.getElementById('miinput')
+let pantalla = document.getElementById('miinput');
 
 
 function mostrar(value){
     if(pantalla.value === '0' && value == 0){
-        pantalla.value = '0'
+        pantalla.value = '0';
     }else if(pantalla.value == '0' && value >= 0){
-        pantalla.value = ''
-        pantalla.value += value
+        pantalla.value = '';
+        pantalla.value += value;
     }else if(pantalla.value == 'Syntax ERROR'){
-        pantalla.value = 'Syntax ERROR'
+        pantalla.value = 'Syntax ERROR';
     }else{
-    pantalla.value += value
+        pantalla.value += value;
     }
 }
 
 function limpiar(){
-    pantalla.value = '0'
+    pantalla.value = '0';
 }
 
 function eliminar(){
     if(pantalla.value.length == 1){
-        pantalla.value = '0'
+        pantalla.value = '0';
     }else if(pantalla.value == 'Syntax ERROR'){
-        pantalla.value = 'Syntax ERROR'
-    }else if(pantalla.value != 0 || pantalla.value.length > 1) {
-        pantalla.value = pantalla.value.slice(0, pantalla.value.length-1)
+        pantalla.value = 'Syntax ERROR';
+    }else if(pantalla.value != 0 || pantalla.value.length > 1){
+        pantalla.value = pantalla.value.slice(0, pantalla.value.length-1);
     }
 }
 
 function resultado(){
     try{
-        pantalla.value = eval(pantalla.value)
+        pantalla.value = eval(pantalla.value);
     }catch{
-        pantalla.value = 'Syntax ERROR'
+        pantalla.value = 'Syntax ERROR';
     }
 }
